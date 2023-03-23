95384f878d925ef4492914dc6528304e8008fd4f
Sara Adams <sara.e.adams@gmail.com>
2023-03-23 15:23:09 +0100

    [infra] Update lint script to use rlocation
    Signed-off-by: Sara Adams <sara.e.adams@gmail.com>

--- infra/lint.sh
+++ infra/lint.sh
@@ -30,6 +30,7 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
+runfiles_export_envvars
 
 function help() {
   cat <<-'EOT'
@@ -47,10 +48,8 @@ Examples:
 EOT
 }
 
-# rlocation returns the wrong path (does not exist), and `bazel run` runs this
-# tool in a runfiles directory, so we use pwd instead.
-JAVA_FORMATTER="$(pwd)/third_party/google-java-format/google-java-format"
-STARLARK_FORMATTER="$(pwd)/third_party/buildifier/buildifier.exe"
+JAVA_FORMATTER="$(rlocation com_engflow_bazel_invocation_analyzer/third_party/google-java-format/google-java-format)"
+STARLARK_FORMATTER="$(rlocation com_engflow_bazel_invocation_analyzer/third_party/buildifier/buildifier.exe)"
 POSITIONAL_ARGS=()
 FIX=
 
