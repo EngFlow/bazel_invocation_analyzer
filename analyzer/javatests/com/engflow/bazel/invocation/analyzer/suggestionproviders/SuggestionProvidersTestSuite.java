469158834ac95729cde7e0efa4cb715b2321a1c2
Sara Adams <sara.e.adams@gmail.com>
2023-11-27 19:47:44 +0100

    Remove unwanted change
    Signed-off-by: Sara Adams <sara.e.adams@gmail.com>

--- analyzer/javatests/com/engflow/bazel/invocation/analyzer/suggestionproviders/SuggestionProvidersTestSuite.java
+++ analyzer/javatests/com/engflow/bazel/invocation/analyzer/suggestionproviders/SuggestionProvidersTestSuite.java
@@ -29,7 +29,6 @@ import org.junit.runners.Suite;
   LocalActionsWithRemoteExecutionSuggestionProviderTest.class,
   MergedEventsSuggestionProviderTest.class,
   NegligiblePhaseSuggestionProviderTest.class,
-  NoCacheActionsSuggestionProviderTest.class,
   QueuingSuggestionProviderTest.class,
   UseSkymeldSuggestionProviderTest.class,
 })
