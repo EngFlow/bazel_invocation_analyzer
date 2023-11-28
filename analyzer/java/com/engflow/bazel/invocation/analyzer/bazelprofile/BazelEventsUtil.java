aaa111ce8f921d7ea7dffda801134254a455759c
Sara Adams <4410333+saraadams@users.noreply.github.com>
2023-11-28 15:49:54 +0100

    [DataProvider] Detect additional "remote download output" events (#137)
    Extend the analysis to find further events that indicate outputs were
    downloaded from a remote resource. This improves the detection of
    remotely executed events.
    
    ---------
    
    Signed-off-by: Sara Adams <sara.e.adams@gmail.com>

--- analyzer/java/com/engflow/bazel/invocation/analyzer/bazelprofile/BazelEventsUtil.java
+++ analyzer/java/com/engflow/bazel/invocation/analyzer/bazelprofile/BazelEventsUtil.java
@@ -32,8 +32,8 @@ public final class BazelEventsUtil {
   /** The event indicates that an action was executed locally. */
   public static boolean indicatesLocalExecution(CompleteEvent event) {
     return CAT_LOCAL_ACTION_EXECUTION.equals(event.category)
-        || CAT_GENERAL_INFORMATION.equals(event.category)
-            && COMPLETE_SUBPROCESS_RUN.equals(event.name);
+        || (CAT_GENERAL_INFORMATION.equals(event.category)
+            && COMPLETE_SUBPROCESS_RUN.equals(event.name));
   }
 
   /** The event indicates that an action was executed remotely. */
@@ -54,7 +54,13 @@ public final class BazelEventsUtil {
    * after an action was executed remotely.
    */
   public static boolean indicatesRemoteDownloadOutputs(CompleteEvent event) {
-    return CAT_REMOTE_OUTPUT_DOWNLOAD.equals(event.category);
+    // See
+    // https://github.com/bazelbuild/bazel/blob/4a29f0851d1cde0240793cdc7a2e2cab926d31b7/src/main/java/com/google/devtools/build/lib/profiler/ProfilerTask.java#L88
+    // and
+    // https://github.com/bazelbuild/bazel/blob/b2cca31705419ba1cd3744c23be4b1d9bdb5c467/src/main/java/com/google/devtools/build/lib/remote/RemoteExecutionService.java#L1211C79-L1211C79
+    return CAT_REMOTE_OUTPUT_DOWNLOAD.equals(event.category)
+        || (CAT_GENERAL_INFORMATION.equals(event.category)
+            && BazelProfileConstants.COMPLETE_REMOTE_DOWNLOAD.equals(event.name));
   }
 
   /** The event documents uploading outputs to a remote cache. */
